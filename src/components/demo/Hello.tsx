b32ba2761f5bf64a4489e23d26b845a27ca82426
Apple <814712074@qq.com>
2019-08-30 20:52:58 +0800

    创建有类型约束的函数组件和类组件

--- src/components/demo/Hello.tsx
+++ src/components/demo/Hello.tsx
@@ -2,9 +2,35 @@ import React from 'react'
 import { Button } from 'antd'
 
 interface Greeting {
-    name: string
+    name: string,
+    // 加了类型约束，设置默认属性，为可选属性才不报错
+    // firstName?: string,
+    // lastName?: string,
+    firstName: string,
+    lastName: string
 }
 
 const Hello = (props: Greeting) => <Button>Hello, {props.name}</Button>
 
+// // 类型约束的函数组件
+// const Hello: React.FC<Greeting> = ({
+//     name,
+//     firstName,
+//     lastName,
+//     children
+// }) => <Button>Hello, {name}</Button>
+
+// 默认树形
+Hello.defaultProps = {
+    firstName: '',
+    lastName: ''
+}
+
+
 export default Hello
+
+/** 
+ * 总结：
+ * 类型约束函数组件没有带来明显好处，还可能官方去除
+ * 
+*/
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/demo/Hello';
+import HelloClass from './components/demo/HelloClass'
 
-ReactDOM.render(<Hello name="ts" />, document.getElementById('root'));
+ReactDOM.render(<HelloClass name="ts" />, document.getElementById('root'));
 
 
