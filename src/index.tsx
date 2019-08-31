5ef07983b530216baf1ecdf4ae3c0209e5c399fe
Apple <814712074@qq.com>
2019-08-31 20:42:48 +0800

    项目实战（路由，mock，axios封装，请求代理）

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/demo/Hello';
-import HelloClass from './components/demo/HelloClass'
-import HelloHoc from './components/demo/HelloHOC'
-import HelloHooks from './components/demo/HelloHooks'
+// import Hello from './components/demo/Hello';
+// import HelloClass from './components/demo/HelloClass'
+// import HelloHoc from './components/demo/HelloHOC'
+// import HelloHooks from './components/demo/HelloHooks'
+import Root from './routers'
 
 ReactDOM.render(
     // <HelloClass name="ts" />,
     // <HelloHoc name="ts" loading={false} />,
-    <HelloHooks name="ts"/>,
+    // <HelloHooks name="ts"/>,
+    <Root />,
     document.getElementById('root')
 );
 
---
+++ src/setupProxy.js
@@ -0,0 +1,14 @@
+/**
+ * create-react-app启动时，自动执行这个文件
+ *  */ 
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(proxy('/api/**/*.action', {
+        target: 'http://localhost:4000',
+        pathRewrite(path) {
+            return path.replace('/api', '/').replace('.action', '.json');
+        }
+    }));
+};
