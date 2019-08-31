5d79e45c2abcc584f8b9618bb98f8068953846c8
Apple <814712074@qq.com>
2019-08-31 14:55:25 +0800

    有类型约束的高阶组件

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './components/demo/Hello';
 import HelloClass from './components/demo/HelloClass'
+import HelloHoc from './components/demo/HelloHOC'
 
-ReactDOM.render(<HelloClass name="ts" />, document.getElementById('root'));
+ReactDOM.render(
+    // <HelloClass name="ts" />,
+    <HelloHoc name="ts" loading={false} />,
+    document.getElementById('root')
+);
 
 
